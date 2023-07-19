c7d84522cb8267419d73db5a12dc2c669da7e3a0
Janzen Faidiban <janzenfaidiban@gmail.com>
2023-07-19 21:03:16 +0900

    ADDED: image background header

--- resources/views/visitor/partials/header.blade.php
+++ resources/views/visitor/partials/header.blade.php
@@ -1,10 +1,10 @@
-<header class="bg-dark py-5">
+<header class="bg-dark py-5" style="background-image: url('https://img.freepik.com/premium-photo/businessman-touching-heart-shape-service-mind-healthcare-business-graph-growth_34200-581.jpg?w=1380');background-size:cover;background-position: center bottom;filter: FlipH;">
     <div class="container px-5">
         <div class="row gx-5 align-items-center justify-content-center">
             <div class="col-lg-8 col-xl-7 col-xxl-6">
                 <div class="my-5 text-center text-xl-start">
-                    <h1 class="display-5 fw-bolder text-white mb-2">{{ env('APP_NAME') ?? '' }}</h1>
-                    <p class="lead fw-normal text-white-50 mb-4">
+                    <h1 class="display-5 fw-bolder text-success mb-2">{{ env('APP_NAME') ?? '' }}</h1>
+                    <p class="lead fw-normal text-success-50 mb-4">
                         {{ env('APP_DESCRIPTION') ?? '' }}
                     </p>
                     <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
